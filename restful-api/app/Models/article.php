38936e2902fea6f3b4b9be45d402c9153cb654d8
felixirw25 <dwikisulton@gmail.com>
2022-06-30 13:34:26 +0700

    setting model

--- restful-api/app/Models/article.php
+++ restful-api/app/Models/article.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class article extends Model
 {
     use HasFactory;
+    protected $table = 'articles';
+    protected $guarded = ['id'];
+
+    public function category()
+    {
+
+        return $this->belongsTo(Category::class);
+    }
+
+    public function user()
+    {
+
+        return $this->belongsTo(User::class);
+    }
 }
--- restful-api/app/Models/category.php
+++ restful-api/app/Models/category.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class category extends Model
 {
     use HasFactory;
+    protected $table = 'categories';
+    protected $guarded = ['id'];
+
+    public function articles()
+    {
+
+        return $this->hasMany(Article::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
